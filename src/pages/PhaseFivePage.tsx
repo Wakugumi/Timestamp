3f0c5dd68890271e023584c1995028930763dccd
Ananda Risyad <anandarisyad@proton.me>
2025-05-06 19:33:19 +0700

    phase five small fix

--- src/pages/PhaseFivePage.tsx
+++ src/pages/PhaseFivePage.tsx
@@ -114,7 +114,7 @@ export default function PhaseFivePage() {
     return (
       <>
         <div
-          className={`min-h-lvh max-h-lvh min-w-full overflow-hidden ${state === State.PROCESSING ? "bg-white" : "bg-inverse-surface"} text-inverse-on-surface
+          className={`min-h-lvh max-h-lvh min-w-full overflow-hidden bg-inverse-surface text-inverse-on-surface
             flex flex-col justify-evenly items-center p-4 gap-2`}
         >
           <div
